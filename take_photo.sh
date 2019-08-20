7406b9ceffc2eddb194bada3236e5326f78c9e39
Stefan Ilie <stefan.ilie@hostmaker.co>
2019-08-20 21:03:29 +0300

    add logging

--- take_photo.sh
+++ take_photo.sh
@@ -5,26 +5,26 @@ generate_name() {
 
 kill_gphoto2(){
 	PID=`ps -eaf | grep gphoto2 | grep -v grep | awk '{print $2}'`
-	echo "killing $PID"
 	kill -INT $PID
+	echo "killed $PID"
 }
 
 take_photo_and_upload(){
-	gphoto2 --capture-image-and-download --filename $FILE
+	GPHOTO_ERROR=$(gphoto2 --capture-image-and-download --filename $FILE) || python3 send_message.py "Problema cu captarea pozei: $GPHOTO_ERROR"
+	echo "Successfully taken $FILE" 
 	python addFrame.py $FILE
 	echo "Uploading photo to gdrive..."
 	LINK="$(gdrive upload $FILE --share)"
-	python print.py $FILE $LINK
+	python print.py $FILE $LINK || python3 send_message.py "Problema cu printarea"
 	#python test_input.py $LINK
 }
 
 restart_preview(){
-	gphoto2 --capture-movie --stdout> fifo.mjpg &
+	gphoto2 --capture-movie --stdout> fifo.mjpg & 
 	omxplayer fifo.mjpg --live
 }
 
-generate_name
-echo "Successfully taken $FILE"
-kill_gphoto2
-take_photo_and_upload
-restart_preview
+(GENERATE_OUTPUT=$(generate_name) || python3 send_message.py "Problema cu generarea de nume de fisier: $GENERATE_OUTPUT") &&
+(KILL_GPHOTO2=$(kill_gphoto2) || python3 send_message.py "Problema cu inchiderea gphoto2: $KILL_GPHOTO2") &&
+(TAKE_PHOTO=$(take_photo_and_upload) || python3 send_message.py "Problema cu upload: $TAKE_PHOTO") &&
+(RESTART=$(restart_preview) || python3 send_message.py "Problema cu pornirea preview-ului: $RESTART")

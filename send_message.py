f027a7ce3f15c5f457ee0d83aa6075871d298a16
Stefan Ilie <stefan.ilie@hostmaker.co>
2019-08-20 16:49:12 +0300

    add telegram bot

---
+++ send_message.py
@@ -0,0 +1,16 @@
+import sys
+import json 
+import requests
+
+from config import TOKEN, URL, CHANNEL_ID
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+send_message(sys.argv[1], CHANNEL_ID)

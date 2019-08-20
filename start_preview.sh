0e1d77dc96d32e1241956d30cad83c150a0b9cc8
Stefan Ilie <stefan.ilie@hostmaker.co>
2019-08-20 22:55:56 +0300

    add gdrive logging, GPIO minitoring

--- start_preview.sh
+++ start_preview.sh
@@ -1,2 +1,3 @@
+python3 monitor_button.py &
 gphoto2 --capture-movie --stdout> fifo.mjpg &
 omxplayer fifo.mjpg --live
--- take_photo.sh
+++ take_photo.sh
@@ -16,7 +16,7 @@ take_photo_and_upload(){
 	echo "Successfully taken $FILE" 
 	python addFrame.py $FILE
 	echo "Uploading photo to gdrive..."
-	LINK="$(gdrive upload $FILE --share)"
+	LINK="$(gdrive upload $FILE --share)" || python3 send_message.py "Problema cu uploadul pozei: $LINK"
 	python print.py $FILE $LINK || python3 send_message.py "Problema cu printarea"
 	#python test_input.py $LINK
 }

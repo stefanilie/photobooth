43dd91e070be788837ea136b0456228f5e368179
Stefan Ilie <ilie.stefan92@yahoo.com>
2019-08-21 09:07:58 +0100

    add photobooth status report

---
+++ photobooth_status.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import json 
+import requests
+
+from config import URL, CHANNEL_ID
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+def send_message(text):
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode={}".format(text, CHANNEL_ID, "markdown")
+    get_url(url)
+
+
+path, dirs, files = next(os.walk(os.getcwd()))
+file_count = len(files)-21
+send_message("`Poze facute:` "+str(file_count))

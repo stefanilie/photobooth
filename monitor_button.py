d8e3609df2eb0d7ddf9bca6384cba1ed7c746b09
Stefan Ilie <stefan.ilie@hostmaker.co>
2019-08-21 21:17:16 +0300

    add anti-flodding

--- monitor_button.py
+++ monitor_button.py
@@ -2,23 +2,33 @@ import os
 import time
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-called_once=False
+class Monitor:
+    def __init__:
+        called_once=False
 
-def button_callback(channel):
-    if not called_once:
-        called_once=True
-        os.system("./take_photo.sh")
+    def start_monitor():
+        GPIO.setwarnings(False) # Ignore warning for now
+        GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+        GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+        GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback) # Setup event on pin 10 rising edge
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+        print("waiting for button press")
 
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback) # Setup event on pin 10 rising edge
+        while not self.called_once:
+            time.sleep(1000000)
+
+    def button_callback(channel):
+        if not called_once:
+            self.called_once=True
+            GPIO.cleanup() # Clean up
+            os.system("./take_photo.sh")
 
 #message = input("Press enter to quit\n\n") # Run until someone presses enter
 
-while True:
-    time.sleep(1000000)
+def main():
+    monitor = Monitor()
+    monitor.start_monitor()
 
-GPIO.cleanup() # Clean up
+if __name__ == "__main__":
+    main()

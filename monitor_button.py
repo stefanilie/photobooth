97bbd602b375f0f85b88437b4bf5978ecea88d28
Stefan Ilie <stefan.ilie@hostmaker.co>
2019-08-21 21:06:49 +0300

    add anti flodding

--- monitor_button.py
+++ monitor_button.py
@@ -2,14 +2,19 @@ import os
 import time
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
+called_once=False
+
 def button_callback(channel):
-    os.system("./take_photo.sh")
+    if not called_once:
+        called_once=True
+        os.system("./take_photo.sh")
+
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback) # Setup event on pin 10 rising edge
 
 #message = input("Press enter to quit\n\n") # Run until someone presses enter
 

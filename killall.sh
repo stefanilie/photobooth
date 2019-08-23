958d60aad8dc96793b958fb0c7a7262e78ce1d54
Stefan Ilie <ilie.stefan92@yahoo.com>
2019-08-23 10:27:31 +0100

    update killall

--- killall.sh
+++ killall.sh
@@ -1,3 +1,2 @@
-killall python
-killall sh
-killall omxplayer.bin
+for pid in $(ps -ef | grep "monitor_button.py" | awk '{print $2}'); do kill -9 $pid; done
+for pid in $(ps -ef | grep "gphoto2" | awk '{print $2}'); do kill -9 $pid; done

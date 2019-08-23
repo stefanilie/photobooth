d4aaed54d0e2fe4be466647e12ae1395d1e99cd9
Stefan Ilie <stefan.ilie@hostmaker.co>
2019-08-23 14:52:40 +0300

    i give up

--- photobooth_status.py
+++ photobooth_status.py
@@ -14,17 +14,16 @@ def send_message(text):
     url = URL + "sendMessage?text={}&chat_id={}&parse_mode={}".format(text, CHANNEL_ID, "markdown")
     get_url(url)
 
-path, dirs, files = next(os.walk(os.getcwd()))
-with open('photos.txt','w') as f:
-    pdb.set_trace()
-    data = f.read()
-    file_count = len(files)-25
-    #f.seek(0)
-    new=int(data)+1
-    f.write(str(new))
-    f.close()
-    if file_count == 175 or file_count == 105:
-      send_message("`Poze facute:` "+str(file_count))
-      send_message("SCHIMBA ROLA!")
-    if file_count % 25 == 0:
-      send_message("`Poze facute:` "+str(file_count))
+path, dirs, files = next(os.walk(os.getcwd())) 
+f=open('photos.txt', 'rw+')
+pdb.set_trace()
+data = f.read()
+file_count = len(files)-25
+#f.seek(0)
+new=int(data)+1
+f.write(str(new))
+if file_count == 175 or file_count == 105:
+  send_message("`Poze facute:` "+str(file_count))
+  send_message("SCHIMBA ROLA!")
+if file_count % 25 == 0:
+  send_message("`Poze facute:` "+str(file_count))
